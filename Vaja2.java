fb428039209ede32b6acebbcb1940c685c5ad338
grimove <47428475+grimove@users.noreply.github.com>
2019-02-07 19:02:46 +0100

    Vaja2

---
+++ Vaja2.java
@@ -0,0 +1,16 @@
+public class Vaja2{
+  public static void main (String[]args) {
+    int stevilo = 10;
+
+    System.out.println("Stevilo je " + stevilo);
+
+
+
+    // int mora imeti obvezno unikatno ime!! to je stevilo npr.
+    // ce bi dali v navednice besedo bi izpisalo samo besedo stevilo
+    // ce napisemo "Stevilo je " + stevilo potem izpise vrednost
+    // "Stevilo je " + stevilo + "." za piko izpisat
+    // /* je vec vrsticni komentar */
+
+  }
+}
